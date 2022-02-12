8a860d0944003442afcd5fb09a6a4f81488d9167
rirefat <rirefat.official@gmail.com>
2022-02-12 21:28:37 +0600

    Done Calculation Functionality

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function clearAll(){
     document.getElementById('display').value ="";
 }
+
+function calculate(input){
+    let num = document.getElementById('display').value += input;
+}
+function result (number){
+    let output = document.getElementById('display').value;
+    let finalOutput = eval(output);
+    document.getElementById('display').value = finalOutput;
+}
+
+
+function clearOne(){
+    display.value = display.value.slice(0,-1); 
+}
